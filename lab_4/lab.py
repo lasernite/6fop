45501af24077f2ff8f332824a633b85ddcb96d95
Laser Nite <lasernite@gmail.com>
2016-02-25 02:11:16 -0500

    bfs for last part in progress

--- lab_4/lab.py
+++ lab_4/lab.py
@@ -43,5 +43,72 @@ def get_actors_with_bacon_number(data, n):
 	return nth_degree_relations
 
 
+def build_relationship_dictionary(data):
+	relationships = {}
+
+	# Build all relationships
+	for film_tuple in data:
+		if film_tuple[0] in relationships.keys():
+			relationships[film_tuple[0]] += [film_tuple[1]]
+		else:
+			relationships[film_tuple[0]] = [film_tuple[1]]
+
+		if film_tuple[1] in relationships.keys():
+			relationships[film_tuple[1]] += [film_tuple[0]]
+		else:
+			relationships[film_tuple[1]] = [film_tuple[0]]
+
+	# Remove Duplicates and Parents
+	for actor_id in relationships:
+		# remove duplicates
+		relationships[actor_id] = list(set(relationships[actor_id]))
+		# remove parent if present
+		if actor_id in relationships[actor_id]:
+			relationships[actor_id].remove(actor_id)
+
+	# remember multi-step loops may still exist, ex. a -> b -> c -> a
+	return relationships
+
+
+def bfs(data, actor_id):
+	relationships = build_relationship_dictionary(data)
+
+	queue = []
+	# Append Kevin Bacon Start
+	queue.append([4724]) 
+
+	# Search until queue is empty
+	while queue:
+		path = queue.pop(0)
+		node = path[-1]
+
+		if node == actor_id:
+			return path
+
+		for children in relationships.get(node, []):
+			new_path = path[:]
+			new_path.append(children)
+			queue.append(new_path)
+
+
 def get_bacon_path(data, actor_id):
-    raise ValueError("Implement me!")
+	print bfs(data, actor_id)
+	
+
+
+	
+	return bfs(data, actor_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
