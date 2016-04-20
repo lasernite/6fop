0c2db91c9a8130c2dca61adf8684a75600c1a671
Laser Nite <lasernite@gmail.com>
2016-04-20 02:33:37 -0400

    lab 9 in progress

--- lab_9/lab.py
+++ lab_9/lab.py
@@ -4,11 +4,33 @@
 Welcome to Boddy Estate! 
 """
 #We suggest making a Player class, too.
+class Player:
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+        self.location = 'foyer'
+
+
 
 class World: 
     
+    # state = {"envelope":[suspect, weapon, room (no order enforced)], 
+    #          "players":[players in order of turn], 
+    #          "playersCards":{"playerName":{"weapons":[weapons list], 
+    #                                        "rooms":[rooms list], 
+    #                                        "suspects":[suspects list]},
+    #                          "playerName2":{"weapons":[weapons list], 
+    #                                        "rooms":[rooms list], 
+    #                                        "suspects":[suspects list]}}}
     def __init__(self, state):
-        return NotImplementedError()
+        self.state = state
+        # Make Player Objects Accessible
+        players = []
+        for playerName in state['players']:
+            players.append(Player(playerName, state['playersCards'][playerName]))
+        self.players = players
+        self.currentPlayer = players[0]
+
         
     #Define any helper methods you'd like, as usual
         
@@ -16,25 +38,25 @@ class World:
     # to peek into your game state. You MUST implement each
     # of these methods, and they must return the proper type
     # according to the readme.
-    
+
     def get_current_player_name(self):
        #Returns the name (string) of the current player
-        return NotImplementedError()
-    
+        return self.currentPlayer.name
+
     def get_players(self):
         #Returns a list of players' names as described in the ReadMe
-        return NotImplementedError()
+        return self.state['players']
 
     def get_location(self, player_string):
         #Returns the location of the specified player
-        return NotImplementedError()
+        return self.state['']
 
     def get_cards(self, player_string):
         # Return a dictionary of cards held by a player
         # with the keys described in the README
         return NotImplementedError()
-    
-    
+
+
     def get_revealed_cards(self):
         #Return a list of revealed cards (order matters!)
         return NotImplementedError
@@ -43,13 +65,20 @@ class World:
         #Return the list of cards seen by this player
         return NotImplementedError
 
-    
+
     #This method controls game play
-    
+
     def take_turn(self, action, args):
     # action is one of {"travel", "guess", "accuse"}
     # arguments are action-dependent. Consult the readme for details
-        return NotImplementedError
-        
-        
+        if action == "travel":
+            # fill in code here, args is a destination like "ballroom"
+            return "travel"
+        elif action == "guess":
+            # fill in code here, args is {"suspect":guess, "room":guess, "weapon":guess}, guess of next player in array
+            # if player not in room in guess, return "invalid room"
+        elif action == "accuse":
+        else:
+            return None
+               
    
