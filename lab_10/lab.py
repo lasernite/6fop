197bc46c04184e81d2f748ebba33ffd00449fb68
Laser Nite <lasernite@gmail.com>
2016-05-02 03:36:36 -0400

    paddle adjusted and ball drifts now with velocity

--- lab_10/lab.py
+++ lab_10/lab.py
@@ -18,7 +18,14 @@ def init(width, height, ball_position, ball_velocity, bricks):
 	return None
 
 def step(time, paddle_1_xpos, paddle_2_xpos, paddle_offset, paddle_radius):
-	return None
+	global ballPosition
+	global ballVelocity
+
+	# New ball position at end of time
+	ballPosition = [ballPosition[0] + ballVelocity[0] * time, ballPosition[1] + ballVelocity[0] * time]
+	print ballPosition
+	
+	return 0
 
 
 # You may assume the canvas is blank
@@ -27,13 +34,15 @@ def draw(canvas, paddle_1_xpos, paddle_2_xpos, paddle_offset, paddle_radius):
 	# canvas.draw_rectangle(upper_left_x, upper_left_y, lower_right_x, lower_right_y, color)
 
 	# Draw player 1 (on bottom)
-	canvas.draw_circle(paddle_1_xpos, boardHeight, paddle_radius, "#FFFFFF")
+	canvas.draw_circle(paddle_1_xpos, boardHeight + paddle_offset, paddle_radius, "#FFFFFF")
 
-	# Draw player 2
-	canvas.draw_circle(paddle_2_xpos, 0, paddle_radius, "#FFFFFF")
+	# Draw player 2 (on top)
+	canvas.draw_circle(paddle_2_xpos, -paddle_offset, paddle_radius, "#FFFFFF")
+
+	# Draw ball
+	canvas.draw_circle(ballPosition[0], ballPosition[1], 5, "#FFFFFF")
 
 	return None
-
 
 
 # Helpers
